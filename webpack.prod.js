2d825a6a3347e5c7a6054435383b4fee1e1381bc
Aaron Opfer <me@aaronopfer.com>
2022-11-28 02:59:49 +0000

    switch to preact to push bundle sizes down more

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,4 +3,12 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'production',
+    resolve: {
+        alias: {
+            "react": "preact/compat",
+            "react-dom/test-utils": "preact/test-utils",
+            "react-dom": "preact/compat",     // Must be below test-utils
+            "react/jsx-runtime": "preact/jsx-runtime"
+        }
+    }
 });
