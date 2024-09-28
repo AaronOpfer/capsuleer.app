9a7182b33a5ae6a4aa31adb3e642566e6541943a
Aaron Opfer <me@aaronopfer.com>
2024-09-28 16:43:45 +0000

    prettier skill requirement tree

--- src/components/skill_requirement_tree.tsx
+++ src/components/skill_requirement_tree.tsx
@@ -4,42 +4,50 @@ import {StaticSkill, skill_data} from "../static_skill_data";
 import {attribute_types, sp_required} from "../misc/sp";
 import {format_duration, format_with_decimals} from "../misc/formatting";
 
-
 interface SkillRequirementTreeProps {
-    skill: StaticSkill,
+    skill: StaticSkill;
 }
 
-export default class SkillRequirementTree extends React.PureComponent<SkillRequirementTreeProps, Record<string, never>> {
-    render () {
+export default class SkillRequirementTree extends React.PureComponent<
+    SkillRequirementTreeProps,
+    Record<string, never>
+> {
+    render() {
         const props = this.props;
-        return (
-            <SkillRequirementTreeNode skill={props.skill} level={1} />
-        );
+        return <SkillRequirementTreeNode skill={props.skill} level={1} />;
     }
 }
 
-
 interface SkillRequirementTreeNodeProps {
-    skill: StaticSkill,
-    level: number,
+    skill: StaticSkill;
+    level: number;
 }
 
 const levelText = ["I", "II", "III", "IV", "V"];
 
-class SkillRequirementTreeNode extends React.PureComponent<SkillRequirementTreeNodeProps, Record<string, never>> {
-    render () {
+class SkillRequirementTreeNode extends React.PureComponent<
+    SkillRequirementTreeNodeProps,
+    Record<string, never>
+> {
+    render() {
         const {skill, level} = this.props;
-        const children = skill.prerequisites.map(prereq => {
+        const children = skill.prerequisites.map((prereq) => {
             return (
-                <SkillRequirementTreeNode key={prereq.skillId} skill={skill_data.skill(prereq.skillId)} level={prereq.skillLevel} />
+                <SkillRequirementTreeNode
+                    key={prereq.skillId}
+                    skill={skill_data.skill(prereq.skillId)}
+                    level={prereq.skillLevel}
+                />
             );
         });
 
         return (
             <div className="skill_requirement_tree_node">
-                <div>{skill.name} {levelText[level-1]}</div>
+                <div>
+                    {skill.name} {levelText[level - 1]}
+                </div>
                 <div>{children}</div>
             </div>
-        )
+        );
     }
 }
